4f98b399d6f47bd56a2ec6f82fe17e07a5c2df4f
montazeri_a <montazeri_a@office.zharfpouyan.net>
2023-04-05 10:45:28 +0330

    ..

--- Cover.py
+++ Cover.py
@@ -1150,8 +1150,9 @@ def main():
     add_handler(CommandHandler('done', finish_editing_tags))
     add_handler(CommandHandler('preview', display_preview))
 
-    add_handler(CallbackQueryHandler(set_language, pattern='persian'))
-    add_handler(CallbackQueryHandler(set_language, pattern='english'))
+    add_handler(CallbackQueryHandler(set_language, pattern=['persian' | 'english']))
+    # add_handler(CallbackQueryHandler(set_language, pattern='english'))
+
     ##########################
     # Admin Command Handlers #
     ##########################
