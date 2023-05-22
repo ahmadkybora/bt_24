e29c2384724216c818333341e5bd578b3e3dae33
montazeri_a <montazeri_a@office.zharfpouyan.net>
2023-05-22 09:07:20 +0330

    ..

--- Cover.py
+++ Cover.py
@@ -99,14 +99,12 @@ def command_start(update: Update, context: CallbackContext) -> None:
         logger.info("A user with id %s has been started to use the bot.", user_id)
 
 def start_over(update: Update, context: CallbackContext) -> None:
-    print(context.user_data)
     reset_user_data_context(context)
-    print(context.user_data)
 
     update.callback_query.message.edit_text(
         translate_key_to(lp.START_OVER_MESSAGE, context.user_data['language']),
         # reply_to_message_id=update.effective_message.message_id,
-        reply_markup=ReplyKeyboardRemove()
+        # reply_markup=ReplyKeyboardRemove()
     )
 
 def command_help(update: Update, context: CallbackContext) -> None:
